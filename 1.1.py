56020859162036a5f65fb0a7c1748ba9c0011574
Krfld <rodrigofldias@gmail.com>
2021-10-30 14:33:36 +0100

    .

--- 1.1.py
+++ 1.1.py
@@ -1,31 +1,41 @@
 import math
 
-#   i   AENDBROAASDSTAETASSELTFRHEIMARLOCAAPOLDIMUELOGIASARSPRO
-#   ii  AMEDRPBOREOIASASDLTPLFAAAEUISSLRGLTHSAAEAARDRCSNEMOTOIO
-
-TEXTO_CLARO = 'abatalhacomospersasteralugarnodesfiladeirodastermopilas'
-ESPESSURA = 6
-
 
 def citala_espartana(texto_claro: str, n: int):
-    # * Verificar espessura da cítala
+    # Verificar espessura da cítala
     if n < 2:
         return 'Espessura da cítala incorreta'
 
-    # * Remover espaços e converter para maiúsculas
+    # Remover espaços e converter para maiúsculas
     texto_claro = texto_claro.replace(' ', '').upper()
 
+    # Obter comprimento do texto
     texto_claro_size = len(texto_claro)
+
+    # Obter largura da cítala (dividir o texto em 'n' partes)
     citala_size = math.ceil(texto_claro_size/n)
 
+    # Percorrer do texto como se fosse uma matriz dividida em 'n' linhas, de cima para baixo, da esquerda para a direita
     texto_cifrado = ''
     for i in range(citala_size):
         for j in range(n):
             k = i + j * citala_size
+
+            # Verificar se há texto (pode acontecer na última linha da matriz)
             if k < texto_claro_size:
                 texto_cifrado += texto_claro[k]
 
     return texto_cifrado
 
+#
+# Testes
+#
 
-print(citala_espartana(TEXTO_CLARO, ESPESSURA))
+
+TEXTO_CLARO = 'abatalhacomospersasteralugarnodesfiladeirodastermopilas'
+
+# i     AENDBROAASDSTAETASSELTFRHEIMARLOCAAPOLDIMUELOGIASARSPRO
+print(citala_espartana(TEXTO_CLARO, 4))
+
+# ii    AMEDRPBOREOIASASDLTPLFAAAEUISSLRGLTHSAAEAARDRCSNEMOTOIO
+print(citala_espartana(TEXTO_CLARO, 6))
--- 2.2.py
+++ 2.2.py
@@ -1,29 +1,39 @@
-#   i       00101011
-#   ii      10000011
-#   iii     11000011
-#   iv      11010011
-
-m = 0b11111110
-k = 0b11010101
-n = 8
-
-
 def cifras_sustituicao_bytes(byte: int, key: int, n: int):
+    # Create mask with sum block size
     mask = int('1'*n, 2)
 
+    # Loop each sum block
     cifra_byte = 0
     for i in range(8//n):
-        # * Shift right to apply mask
+        # Shift right to sum and apply mask
         masked_byte = byte >> i*n
         masked_key = key >> i*n
 
+        # Apply mask on each sum block
         masked_result = (masked_byte + masked_key) & mask
 
-        # * Shift left to build cifra_byte
+        # Shift left to build cifra_byte
         cifra_byte += masked_result << i*n
 
-    # * Convert to binary, remove '0b' and add 0's until 8 bits
+    # Convert to binary, remove '0b' and add 0's until 8 bits
     return bin(cifra_byte).removeprefix('0b').rjust(8, '0')
 
+#
+# Testes
+#
 
-print(cifras_sustituicao_bytes(m, k, n))
+
+M = 0b11111110
+K = 0b11010101
+
+# i     00101011
+print(cifras_sustituicao_bytes(M, K, 1))
+
+# ii    10000011
+print(cifras_sustituicao_bytes(M, K, 2))
+
+# iii   11000011
+print(cifras_sustituicao_bytes(M, K, 4))
+
+# iv    11010011
+print(cifras_sustituicao_bytes(M, K, 8))
