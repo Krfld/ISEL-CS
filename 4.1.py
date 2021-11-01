0cad53321aa12f0322f1cbc236723f23e32bf808
Krfld <rodrigofldias@gmail.com>
2021-11-01 00:28:15 +0000

    .

--- 4.1.py
+++ 4.1.py
@@ -5,8 +5,8 @@ l = s - n
 
 
 def hash_MD_funcaof(m: str, H0: str):
-    H = H0
-    blocks = len(m) // l
+    H = [H0]
+    blocks = len(m)//l
 
     # Split 'm' into blocks
     mi = [m[i*l:i*l+l] for i in range(blocks+1)]
@@ -19,21 +19,21 @@ def hash_MD_funcaof(m: str, H0: str):
     mi += [bin(l).removeprefix('0b').rjust(l, '0')]
 
     # Apply 'f'
-    for m in mi:
+    for i in range(len(mi)):
         # Split each 'm' into blocks of 4 bits
-        B = [(H + m)[i*4:i*4+4] for i in range(4)]
+        B = [(H[i] + mi[i])[j*4:j*4+4] for j in range(4)]
 
         # Sum each block
-        for i in range(3):
-            B[i+1] = bin(int(B[i], 2)+int(B[i+1], 2)).removeprefix('0b')[-4:]
+        for j in range(3):
+            B[j+1] = bin(int(B[j], 2)+int(B[j+1], 2)).removeprefix('0b')[-4:]
 
         # Update next H
-        H = B[3]
+        H += [B[3]]
 
-    return H0.rjust(4, '0')
+    return H[len(mi)].rjust(4, '0')
 
 
 m = '010101010101010000'
 H0 = '1000'
 
-hash_MD_funcaof(m, H0)
+print(hash_MD_funcaof(m, H0))
